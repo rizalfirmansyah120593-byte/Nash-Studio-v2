e84f0ef5209a38250bf898b47a63895764de11f9
Rizal Firmansyah <rizalfirmansyah120593@gmail.com>
2026-08-26 08:55:12 +0700

    Inisialisasi awal proyek Nash Studio

---
+++ frontend/src/constants/feedback.js
@@ -0,0 +1,44 @@
+export const feedbackH1LG = [
+    [
+        "Staying at Nash Studio® in the",
+        "California desert redefined my",
+        "retreat — modern design meets",
+        "nature, and every sunset feels",
+        "like a serene masterpiece."
+    ],
+    [
+        "Nash Studio® offered the perfect",
+        "escape — sleek, modern spaces",
+        "surrounded by desert stillness.",
+        "Each moment felt peaceful,",
+        "grounded, and truly unique."
+    ],
+    [
+        "Nash Studio® was the perfect",
+        "desert hideaway — stylish,",
+        "peaceful, and fully surrounded",
+        "by stunning views day and",
+        " night."
+    ]
+];
+
+export const feedbackReviewLG = [
+    [
+        "Marcus Simpson",
+        "New York",
+        "review1",
+        "33%"
+    ],
+    [
+        "Lena Morrison",
+        "Los Angeles",
+        "review2",
+        "66%"
+    ],
+    [
+        "Jason Whitaker",
+        "San Francisco",
+        "review3",
+        "100%"
+    ]
+];
---
+++ frontend/src/constants/welcome.js
@@ -0,0 +1,33 @@
+export const welcomeLinesLG = [
+    "Welcome to a world of wild California",
+    "desert with Nash Studio®, where you will",
+    "discover exquisite nature observing it",
+    "from Nash Studio houses, nestled in the",
+    "one of the most breathtaking",
+    "destination on the United States.",
+];
+export const welcomeLinesSM = [
+    "Welcome to a world",
+    "of wild California",
+    "desert with ",
+    "Nash Studio®, where ",
+    "you will discover ",
+    "exquisite nature",
+    "observing it from",
+    "Nash Studio houses,",
+    "nestled in the one",
+    "of the most",
+    "breathtaking",
+    "destination on the",
+    "United States.",
+];
+
+export const chooseLinesLG =[
+    "Choose the one",
+    "you like best"
+];
+export const chooseLinesSM =[
+    "Choose the",
+    "one you like",
+    "best"
+];
